1adbe64a83f250cf7e88fb683956f4b5edf99fb5
Michal Kukowski <kukossw@gmail.com>
2025-06-26 16:53:51 +0200

    Massive CI and Dockerfiles refactor
    Commit introduces:
    - fix for locale generation (redhat)
    - new tagging system (redhat + ubuntu)
    - huge refactoring of `test_dockerfile_main.sh`
    - user without root for proper testing (redhat + ubuntu)
    - dynamic version fetching for 3pp (redhat)
    - matrix strategy for github action
    - ubuntu latest -> ubuntu 25.04
    - added ubuntu 24.04

--- test/test_dockerfile_main.sh
+++ test/test_dockerfile_main.sh
@@ -1,69 +1,57 @@
 #!/usr/bin/env bash
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_TOP_DIR="${SCRIPT_DIR}/.."
 
-# @brief Function to run a docker test
-#
-# USAGE:
-#   Test::Docker::run_test "Dockerfile path" "Docker tag" "Docker run command"
-# OUTPUT:
-#   Docker build output
-#   Docker run output
-#
-# @param $1 - Dockerfile path
-# @param $2 - Docker tag
-#
-# @return 0 on success, return 1 on failure
 function Test::Docker::run_test()
 {
     local dockerfile_path="${1}"
     local docker_tag="${2}"
 
+    shift 2
+    local docker_arguments=("$@")
+
     DOCKER_BUILDKIT=1 docker build --no-cache --file "${dockerfile_path}" --tag "${docker_tag}" "${PROJECT_TOP_DIR}" || return 1
-    docker run --rm "${docker_tag}" || return 1
+    docker run "${docker_arguments[@]}" --rm "${docker_tag}" || return 1
+    docker image rm "${docker_tag}" || return 1
+
+    return 0
 }
-
-# @brief Function to run all docker tests in parallel
-#
-# USAGE:
-#   Test::Docker::run_all_parallel
-# OUTPUT:
-#   Docker build output
-#
-# @return 0 on success, return 1 on failure
-function Test::Docker::run_all_parallel()
-{
-    (
-        ####################################### TEST UNIT #######################################
-        # Test  redhat ubi8
-        Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_redhat_ubi8" "docker_redhat_ubi8" &
-        docker_pid_1=$!
-
-        # Test ubuntu latest
-        Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_latest" "docker_ubuntu_latest" &
-        docker_pid_2=$!
-
-	wait "${docker_pid_1}" "${docker_pid_2}"
-    )
-
-    if ! some_command; then
-        return 1
-    fi
-}
-
 
 function Test::Docker::run_all()
 {
-    ####################################### TEST UNIT #######################################
-    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_redhat_ubi8" "docker_redhat_ubi8" || return 1
-    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_latest" "docker_ubuntu_latest" || return 1
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_redhat_8" "tester:redhat-8" -e SELECTED_SHELL="bash" || return 1
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_redhat_8" "tester:redhat-8" -e SELECTED_SHELL="zsh" || return 1
+
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2404" "tester:ubuntu-2404" -e SELECTED_SHELL="bash" || return 1
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2404" "tester:ubuntu-2404" -e SELECTED_SHELL="zsh" || return 1
+
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2504" "tester:ubuntu-2504" -e SELECTED_SHELL="bash" || return 1
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/Dockerfile_ubuntu_2504" "tester:ubuntu-2504" -e SELECTED_SHELL="zsh" || return 1
+
+    return 0
 }
 
-# If user specifies the test to run in parallel by using -p flag then run the tests in parallel
-if [[ "${1:-}" == "-p" ]]; then
-    # Run the tests in parallel
-    Test::Docker::run_all_parallel || exit 1
-else
-    # Run the tests sequentially
+if (( $# == 0 )); then
     Test::Docker::run_all || exit 1
+else
+    if (( $# < 2 )); then
+        printf "Usage: %s dockerfile_name dockerimage_tag [optional: dockerimage_arguments]\n" "$0"
+        printf "Example: %s Dockerfile_ubuntu_2404 tester:ubuntu-2404 -e SELECTED_SHELL=\"bash\"\n" "$0"
+        printf "If no arguments are provided, all tests will be run.\n"
+        exit 1
+    fi
+
+    dockerfile_name="${1}"
+    docker_tag="${2}"
+    shift 2
+    docker_arguments=("$@")
+
+    if [[ ! -f "${PROJECT_TOP_DIR}/test/docker/${dockerfile_name}" ]]; then
+        printf "Dockerfile %s does not exist in the test/docker directory." "${dockerfile_name}"
+        exit 1
+    fi
+
+    Test::Docker::run_test "${PROJECT_TOP_DIR}/test/docker/${dockerfile_name}" "${docker_tag}" "${docker_arguments[@]}" || exit 1
 fi
+
